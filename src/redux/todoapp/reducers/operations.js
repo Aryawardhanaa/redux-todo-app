57972a07f1d99659b5a3ab0bd520a1c3a4ba6690
baploarya <aryawardhana7831@gmail.com>
2023-05-26 16:38:44 +0700

    perbaikan

--- src/redux/todoapp/reducers/operations.js
+++ src/redux/todoapp/reducers/operations.js
@@ -1,26 +1,25 @@
 /* eslint-disable array-callback-return */
 import { ADD_TODO, DELETE_ALL, REMOVE_TODO, UPDATE_CHECKBOX, UPDATE_TODO } from "../actions";
 
-const initialState=[
-    {id: 1, todo: 'Buy Laptop', completed: false},
-    {id: 2, todo: 'Master Redux', completed: false},
-    {id: 3, todo: 'Watering Plants', completed: true},
+const initialState = [
+    { id: 1, todo: 'Belik Jemuran', completed: false },
+    { id: 2, todo: 'Belik Bakwan', completed: false },
 ];
 
-export const operationsReducer=(state=initialState, action)=>{
-    switch(action.type){
+export const operationsReducer = (state = initialState, action) => {
+    switch (action.type) {
         case ADD_TODO:
             return [...state, action.payload];
         case DELETE_ALL:
             return [];
         case REMOVE_TODO:
-            const filteredTodos = state.filter((todo)=>todo.id!==action.payload);
+            const filteredTodos = state.filter((todo) => todo.id !== action.payload);
             return filteredTodos;
         case UPDATE_TODO:
             let data = action.payload;
-            const updatedArray=[];
-            state.map((item)=>{
-                if(item.id===data.id){
+            const updatedArray = [];
+            state.map((item) => {
+                if (item.id === data.id) {
                     item.id = data.id;
                     item.todo = data.todo;
                     item.completed = data.completed;
@@ -29,9 +28,9 @@ export const operationsReducer=(state=initialState, action)=>{
             })
             return updatedArray;
         case UPDATE_CHECKBOX:
-            let todoArray=[];
-            state.map((item)=>{
-                if(item.id===action.payload){
+            let todoArray = [];
+            state.map((item) => {
+                if (item.id === action.payload) {
                     item.completed = !item.completed;
                 }
                 todoArray.push(item);
